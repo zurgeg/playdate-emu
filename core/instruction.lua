c07c219ea7108dd6ea135e6c749db10244b9993d
zurgeg <46549042+zurgeg@users.noreply.github.com>
2022-05-18 21:21:58 -0400

    stuff

--- core/instruction.lua
+++ core/instruction.lua
@@ -16,32 +16,32 @@ function bitand(a, b)
 end
 
 function handleInstruction(instruction)
-    --print("instruction:" .. instruction .. " savedPC:" .. savedPC .. " currentPC:" .. currentPC)
-    --print("first nybble:" .. (bitand(instruction, 0x00F0)) .. " second nybble:" .. (instruction & 0x0F00))
-    --if (instruction & 0x00F0 == 0x0 and not instruction & 0xF000 == 0xE) then
-    --    -- JSR NNN: Jump to subroutine NNN
-    --    savedPC = currentPC -- save PC
-    --    currentPC = instruction & 0x0FFF -- jump to sub address
-    --    isSub = true -- set subroutine flag
-    --elseif instruction == 0x00E0 then
-    --    -- CLS: Clear the display
-    --    playdate.graphics.clear()
-    --elseif instruction == 0x00EE and isSub then
-    --    -- RET: Return from subroutine
-    --    currentPC = savedPC -- jump back to saved PC
-    --    isSub = false -- clear subroutine flag
-    --elseif instruction & 0x00F0 == 0x1 then
-    --    -- JP NNN: Jump to address NNN
-    --    print("jp: " .. instruction & 0xFF0F)
-    --    currentPC = instruction & 0x0FF0F
-    --elseif instruction & 0xF == 0x2 then
-    --    -- CALL NNN: Call subroutine at NNN
-    --    savedPC = currentPC -- save PC
-    --    currentPC = instruction & 0x0FFF -- jump to sub address
-    --    isSub = true -- set subroutine flag
-    --else
-    --    print("Unknown instruction: " .. instruction)
-    --end
+    print("instruction:" .. instruction .. " savedPC:" .. savedPC .. " currentPC:" .. currentPC)
+    print("first nybble:" .. (bitand(instruction, 0x00F0)) .. " second nybble:" .. (instruction & 0x0F00))
+    if (instruction & 0x00F0 == 0x0 and not instruction & 0xF000 == 0xE) then
+        -- JSR NNN: Jump to subroutine NNN
+        savedPC = currentPC -- save PC
+        currentPC = instruction & 0x0FFF -- jump to sub address
+        isSub = true -- set subroutine flag
+    elseif instruction == 0x00E0 then
+        -- CLS: Clear the display
+        playdate.graphics.clear()
+    elseif instruction == 0x00EE and isSub then
+        -- RET: Return from subroutine
+        currentPC = savedPC -- jump back to saved PC
+        isSub = false -- clear subroutine flag
+    elseif instruction & 0x00F0 == 0x1 then
+        -- JP NNN: Jump to address NNN
+        print("jp: " .. instruction & 0xFF0F)
+        currentPC = instruction & 0x0FF0F
+    elseif instruction & 0xF == 0x2 then
+        -- CALL NNN: Call subroutine at NNN
+        savedPC = currentPC -- save PC
+        currentPC = instruction & 0x0FFF -- jump to sub address
+        isSub = true -- set subroutine flag
+    else
+        print("Unknown instruction: " .. instruction)
+    end
     return currentPC
 end
 
